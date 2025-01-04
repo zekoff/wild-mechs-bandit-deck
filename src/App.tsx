d6a5eb62a4a999d410a5888663e3b62b3873de87
Michael Zekoff <zekoff@gmail.com>
2025-01-04 14:59:59 -0600

    Correctly display table data

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 import banditDeck from './assets/bandit_deck.json'
 import { Box, Card, CardContent, Container, Typography } from '@mui/material'
 
-function shuffleArray(array: string[][]) {
+function shuffleArray(array: {}[]) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -12,7 +12,7 @@ function shuffleArray(array: string[][]) {
   return array;
 }
 
-function sumTableValues(deck: { table: { [key: string]: number|string|null }[] }) {
+function sumTableValues(deck: { table: { [key: string]: number | string | null }[] }) {
   const sums: { [key: string]: number } = {};
   deck.table.forEach(row => {
     for (const key in row) {
@@ -29,11 +29,11 @@ function sumTableValues(deck: { table: { [key: string]: number|string|null }[] }
 
 function App() {
   const [cardIndex, setCardIndex] = useState(0);
-  const cards = shuffleArray(banditDeck.list);
+  const cards = shuffleArray(banditDeck.table);
   const [currentCard, setCurrentCard] = useState(cards[cardIndex]);
   const cycleCard = () => {
     const nextCardIndex = cardIndex + 1;
-    if (nextCardIndex < banditDeck.cards.length) {
+    if (nextCardIndex < cards.length) {
       setCardIndex(nextCardIndex);
       setCurrentCard(cards[nextCardIndex]);
     } else {
@@ -53,8 +53,12 @@ function App() {
       <Typography variant="h3">Cards Remaining: {getCardsRemaining()}</Typography>
       <Card>
         <CardContent>
-          {currentCard.map(
-            (element, index) => <Typography key={index} variant="body1">{element}</Typography>
+          {Object.entries(currentCard).map(
+            ([key, value]) => {
+              if (!value) return null;
+              if (key === 'Gold') return <Typography key={key} variant="body1">Gold to {value as string}</Typography>;
+              return <Typography key={key} variant="body1">{value as string} to {key}</Typography>;
+            }
           )}
         </CardContent>
         <button disabled={!getCardsRemaining()} onClick={cycleCard}>Draw Card</button>
